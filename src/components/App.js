5280ddb968b6a7c1d4b13ca4a81d898c46900e23
Sudip Ghosh <ghoshsudip084@gmail.com>
2020-10-18 22:53:42 +0530

    Change3

--- src/components/App.js
+++ src/components/App.js
@@ -2,13 +2,13 @@ import React, {Component, useState} from "react";
 import '../styles/App.css';
 
 class App extends Component {
+    relatives = ["Sg","Sf","Sk","Sd"];
     render() {
-        const relatives = ["Sg","Sf","Sk","Sd"];
         return(
            <>
     <ol key="relativeList">
         {
-        relatives.map((el,index) =>{
+        this.relatives.map((el,index) =>{
             <li key={`relativeListItem${index+1}`}>
                 {el}
             </li>
